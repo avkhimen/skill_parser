e8012e8ffb0e2fd5f00bc4962a9bc97a9077d9a7
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-13 05:43:35 +0000

    Adding load_env

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import numpy as np
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
 
-llm = ChatOpenAI(openai_api_key="...")
+load_dotenv()
+
+print(os.getenv('OPENAI_API_KEY'))
+
+llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are world class technical documentation writer."),

79ab8e01a01749ffb20c7df01730a002b3f494e8
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-14 06:06:21 +0000

    Adding more examples

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-import numpy as np
+# import numpy as np
+# from langchain_openai import ChatOpenAI
+# from langchain_core.prompts import ChatPromptTemplate
+# import os
+# from dotenv import load_dotenv
+# from langchain_core.output_parsers import StrOutputParser
+
+# load_dotenv()
+
+# output_parser = StrOutputParser()
+
+# print(os.getenv('OPENAI_API_KEY'))
+
+# llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are world class technical documentation writer."),
+#     ("user", "{input}")
+# ])
+
+# chain = prompt | llm | output_parser
+
+# chain.invoke({"input": "how can langsmith help with testing?"})
+
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-import os
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-print(os.getenv('OPENAI_API_KEY'))
+api_key = os.getenv('OPENAI_API_KEY')
 
-llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
+# chat_model = ChatOpenAI(openai_api_key=api_key)
+
+# result = chat_model.invoke('Hi')
+
+# print(result)
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(openai_api_key=api_key)
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are world class technical documentation writer."),
     ("user", "{input}")
 ])
+
+chain = prompt | llm
+
+result = chain.invoke({"input": "how can langsmith help with testing?"})
+
+print(result)

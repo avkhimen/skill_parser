4360bb3b0e0f74c8ee03e26aaecf6175f461a791
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-15 03:52:06 +0000

    Adding json

--- main.py
+++ main.py
@@ -22,7 +22,7 @@
 
 # chain.invoke({"input": "how can langsmith help with testing?"})
 
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
@@ -30,28 +30,66 @@ load_dotenv()
 
 api_key = os.getenv('OPENAI_API_KEY')
 
-# chat_model = ChatOpenAI(openai_api_key=api_key)
+# # chat_model = ChatOpenAI(openai_api_key=api_key)
 
-# result = chat_model.invoke('Hi')
+# # result = chat_model.invoke('Hi')
+
+# # print(result)
+
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(openai_api_key=api_key)
+
+# ad_body, skills_lst = [], []
+
+# llm_input = [ad_body, skills_lst]
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are world class technical documentation writer."),
+#     ("user", "{input}")
+# ])
+
+# chain = prompt | llm
+
+# result = chain.invoke({"input": "how can langsmith help with testing?"})
 
 # print(result)
 
-from langchain_core.prompts import ChatPromptTemplate
+
 from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+import json
 
-llm = ChatOpenAI(openai_api_key=api_key)
+chat_llm = ChatOpenAI(temperature=0.0)
 
-ad_body, skills_lst = [], []
+template_string = """You are a master branding consulatant who specializes in naming brands. \
+You come up with catchy and memorable brand names.
 
-llm_input = [ad_body, skills_lst]
+Take the brand description below delimited by triple backticks and use it to create the name for a brand.
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
+brand description: ```{brand_description}```
 
-chain = prompt | llm
+then based on the description and you hot new brand name give the brand a score 1-10 for how likely it is to succeed.
 
-result = chain.invoke({"input": "how can langsmith help with testing?"})
+Format the output as JSON with the following keys:
+brand_name
+likelihood_of_success
+reasoning
+"""
 
-print(result)
+prompt_template = ChatPromptTemplate.from_template(template_string)
+
+# print(prompt_template.messages[0].prompt)
+
+branding_messages = prompt_template.format_messages(brand_description="a cool hip new sneaker brand aimed at rich kids")
+
+# print(branding_messages)
+
+consultant_response = chat_llm(branding_messages)
+
+print(type(consultant_response.content))
+
+rjs = json.loads(consultant_response.content)
+
+print(rjs)
